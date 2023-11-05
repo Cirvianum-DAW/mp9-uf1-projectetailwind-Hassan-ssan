ce47a94dcf30692fa598f83717f6e8b54992d3d7
Hassan Shah <syed.ulhassan@cirvianm.cat>
2023-11-05 21:56:16 +0100

    finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'principal': '#E9EDC9',
+      'secondary': '#292929',
+      'highlight': {
+        500: '#D4A373',
+        800: '#c98541',
+      },
+    },
   },
   plugins: [],
 };
+
+
